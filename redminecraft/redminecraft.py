44cf4df9df21b5c25f3a08a4950b2a33e329022e
schindlershadow <SchindlerShadow@gmail.com>
2016-07-18 02:18:05 -0400

    first try at settings system

--- redminecraft/redminecraft.py
+++ redminecraft/redminecraft.py
@@ -9,36 +9,23 @@ try:
 
 except:
     mcstatusAvailable = False
-    
-servername = "schindlershadow.zapto.org"
-port = "25565"
 
 class Redminecraft:
     """Gets information from a minecraft server!"""
 
     def __init__(self, bot):
         self.bot = bot
-        self.settings = fileIO("data/redminecraft/settings.json", 'load')
-        self.server_specific_setting_keys = ["SERVER", "PORT"]
-        servername = self.settings["SERVER"]
-        port = self.settings["PORT"]
-    
-        if port == "":
-            port = "25565"
-    
     
     @commands.command()
-    async def mycom(self):
+    async def xile(self):
         """This does stuff!"""
-
-        #Your code will go here
-        await self.bot.say("I can do stuff!")
+        await self.bot.say("Xile sucks!")
 
     @commands.command(name = "status")
     async def status(self):
         """Get server status"""
 
-        server = MinecraftServer(servername, int(port))
+        server = getServer()
         status = server.status()
         await self.bot.say("The main Infinix server has {0} players and replied in {1} ms".format(status.players.online, status.latency))
 
@@ -46,7 +33,7 @@ class Redminecraft:
     async def latency(self):
         """Get server latency"""
 
-        server = MinecraftServer(servername, int(port))
+        server = getServer()
         latency = server.ping()
         await self.bot.say("The server replied in {0} ms".format(latency))
 
@@ -54,13 +41,14 @@ class Redminecraft:
     async def players(self):
         """Get server players"""
 
-        server = MinecraftServer(servername, int(port))
+        server = getServer()
         query = server.query()
         await self.bot.say("The server has the following players online: {0}".format(", ".join(query.players.names)))
 
     @commands.command(pass_context=True, name = "setserver")
     @checks.is_owner()
     async def setserver(self, name: str):
+        """Set the server IP/Domain"""
         self.settings["SERVER"] = name
         await self.bot.say("Server set to " + name)
         self.save_settings()
@@ -68,16 +56,44 @@ class Redminecraft:
     @commands.command(pass_context=True, name = "setport")
     @checks.is_owner()
     async def setport(self, name: str):
+        """Set server port if not default"""
         self.settings["PORT"] = name
         await self.bot.say("Port set to " + name)
         self.save_settings()
     
+def getServer():
+    self.settings = fileIO("data/redminecraft/settings.json", 'load')
+    self.server_specific_setting_keys = ["SERVER", "PORT"]
+    servername = self.settings["SERVER"]
+    port = self.settings["PORT"]
+    
+    if port == 0:
+        port = 25565
+        
+    return MinecraftServer(servername, int(port))
+    
 def save_settings(self):
     fileIO('data/redminecraft/settings.json', 'save', self.settings)
+    
+def check_folders():
+    folders = ("data/redminecraft")
+    for folder in folders:
+        if not os.path.exists(folder):
+            print("Creating " + folder + " folder...")
+            os.makedirs(folder)
+    
+def check_files():
+    default = {"SERVER": "8.8.8.8", "PORT": 25565}
+    settings_path = "data/redminecraft/settings.json"
+    if not os.path.isfile(settings_path):
+        print("Creating default audio settings.json...")
+        fileIO(settings_path, "save", default)
 
 def setup(bot):
+    check_folders()
+    check_files()
     if mcstatusAvailable:
-        bot.add_cog(redminecraft(bot))
+        bot.add_cog(Redminecraft(bot))
 
     else:
         raise RuntimeError("You need to run `pip install mcstatus`")
